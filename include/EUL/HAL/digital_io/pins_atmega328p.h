7a54f69c3b656ffe01a0b00aaa312a1439a1a8d3
Tim Herreijgers <timtim3001@gmail.com>
2021-05-28 11:05:44 +0200

    Added copyright notices again

--- include/EUL/HAL/digital_io/pins_atmega328p.h
+++ include/EUL/HAL/digital_io/pins_atmega328p.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Tim Herreijgers
+ * Copyright (c) 2020-2021 Tim Herreijgers
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
