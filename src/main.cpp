6974b177911860f60b1e67642d68d49ee7268201
Tim Herreijgers <timtim3001@gmail.com>
2021-07-07 13:31:32 +0200

    Removed boilerplate code

--- src/main.cpp
+++ src/main.cpp
@@ -17,63 +17,6 @@
 
 #ifndef UNIT_TEST
 
-#if 0
-
-//#define F_CPU 16000000L
-
-#define print(x) while(!usart_println(x))
-
-#include <EUL/EUL.h>
-extern "C" {
-#include <EUL/modules/IRRemote/ir_remote.h>
-}
-#include <util/delay.h>
-#include <inttypes.h>
-
-uint32_t input = DIO_PB1;
-uint32_t led = DIO_PB5;
-
-int main()
-{
-//    logger_addLogger(&usartLogger115200);
-//    logger_init();
-    usart_begin(115200);
-//
-    print("Starting boot procedure");
-    dio_setDirection(led, true);
-    dio_setDirection(input, true);
-    while(!usart_println("Output led direction set"));
-    while(!usart_println("Initializing ir module"));
-    ir_init(0, input);
-    while(!usart_println("Initialized ir module"));
-    while(!usart_println("Finished boot procedure"));
-
-//    ir_sendSamsungRaw(0xE0E0D02F);
-
-    for(;;)
-    {
-        if(ir_available())
-        {
-//            dio_setOutput(led, true);
-            uint32_t data = ir_read();
-//            char hex[32];
-//
-//            sprintf(hex, "0x%" PRIX32, data);
-//            while(!usart_print("Received hex value: "));
-//            while(!usart_println(hex));
-            if(data == 0xE0E0D02F)
-                while(!usart_println("Volume down is pressed"));
-            if(data == 0xE0E0E01F)
-                while(!usart_println("Volume up is pressed"));
-
-
-        }
-        _delay_ms(100);
-//
-    }
-}
-#endif
-
 #include "EUL/EUL.h"
 #include "avr/io.h"
 #include "avr/interrupt.h"
@@ -85,27 +28,12 @@ void callback()
 
 int main()
 {
-//    CLKPR = (1 << CLKPS3);
-
     dio_setDirection(DIO_PB3, true);
-//    dio_setOutput(DIO_PB3, true);
 
-   Timer_t *timer = timer0;
-   timer_initCtc(timer, PRESCALER_1);
-   timer_setCompA(timer, 250);
-   timer_setCallback(timer, ON_MATCH_COMP_A, callback);
-
-    // cli();
-    // // Set mode to CTC
-    // TCCR0A = _BV(WGM01);
-    // // Set compare mode and prescaler
-    // TCCR0B |= _BV(CS02) | _BV(CS00);
-    // // Set max value
-    // OCR0A = 250;
-    // // Setup interrupt
-    // TIMSK |= _BV(OCIE0A) | _BV(OCIE0B) | _BV(TOIE0);
-    // sei();
-
+    Timer_t *timer = timer0;
+    timer_initCtc(timer, PRESCALER_1);
+    timer_setCompA(timer, 250);
+    timer_setCallback(timer, ON_MATCH_COMP_A, callback);
 
     for(;;)
     {
